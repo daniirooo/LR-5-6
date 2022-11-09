c7c87dea9bda930ae2a6ef7f55a586d9f4369b01
daniirooo <117731362+daniirooo@users.noreply.github.com>
2022-11-09 14:42:19 +0300

    Add files via upload

---
+++ zadanie3.pas
@@ -0,0 +1,28 @@
+﻿var i,m,k:integer ; s:array [1..20] of integer;
+begin
+  write('Дан массив ');
+  for i:=1 to 20 do
+  begin
+    s[i]:=random(-52,65);
+    write(s[i],' ');
+  end;
+  writeln();
+  m:=0;
+  for i:=1 to 20 do
+  begin
+    if s[i]>m then
+    begin
+      m:=s[i];
+      k:=i;
+    end;
+  end;
+  writeln('max - ',m,'. number - ',k, '.');
+  for i:=19 downto 1 do if (s[i]<m) and (s[i]>0) then m:=s[i];
+  for i:=1 to 19 do if s[i]=m then writeln('min положительный - ',m,'. number - ',i,'.');
+  for i:=1 to 19 do if (s[i] mod 5 = 0)then
+  begin
+    m:=s[i];
+    k:=i;
+  end;
+  writeln('Номер последнего элемента массива, кратного 5 - ',k,'.') 
+end.
---
+++ енпм.pas
@@ -0,0 +1,21 @@
+﻿program one;
+var
+ mas:array[1..20] of integer;
+ i,s: integer;
+ m: string;
+  begin
+    s:=1;
+    for i:=1 to 20 do
+      begin
+    mas[i]:=random(1,200);
+    write(' ', i,': ',mas[i]);
+    if (mas[i] mod 19 = 0) and (i mod 2 = 1) then
+      s:=s*mas[i];
+write(mas[i]);
+m:=m+chr(mas[i]);
+end;
+writeln(s);
+writeln(m);
+end.
+     
+ 
